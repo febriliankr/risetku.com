919b28f424902b102902075ddff4e9e36f7f5adf
febriliankr <febrilian.kr@gmail.com>
2020-09-16 23:08:47 +0700

    added hitung besar sampel

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Footer from "./components/Footer/Footer";
 import Navbar from "./components/Navbar/Navbar";
+import BesarSampel from "./Pages/BesarSampel/BesarSampel";
 import Landing from "./Pages/Landing/Landing";
 import UjiHipotesis from "./Pages/UjiHipotesis/UjiHipotesis";
 
@@ -13,6 +14,7 @@ function App() {
         <Switch>
           <Route exact path="/" component={Landing}/>
           <Route path="/uji-hipotesis" component={UjiHipotesis}/>
+          <Route path="/hitung-besar-sampel" component={BesarSampel}/>
         </Switch>
         <Footer/>
       </BrowserRouter>
---
+++ src/Pages/BesarSampel/BesarSampel.js
@@ -0,0 +1,47 @@
+import React from "react";
+import "./BesarSampel.css";
+import { Link } from "react-router-dom";
+import { Button } from "../../components/Button/Button";
+
+function BesarSampel() {
+  return (
+    <>
+    <div className="uji-hipotesis__container">
+      <section className="uji-hipotesis__text">
+        <h1 className="uji-hipotesis__text-header gradient-text">
+          Hitung Besar Sampel
+        </h1>
+        <p className="uji-hipotesis__text-description">
+          Temukan rumus besar sampel yang cocok untuk penelitianmu dan hitung
+          jumlahnya dengan menjawab beberapa pertanyaan berikut!
+        </p>
+
+        <div className="uji-hipotesis__buttons">
+          <Link to="/hitung-besar-sampel/calculate">
+            <Button buttonStyle="btn--evoblue">
+              Mulai Hitung Besar Sampel
+            </Button>
+          </Link>
+        </div>
+      </section>
+
+    </div>
+    <div className="subsection__container">
+        <p className="subsection__text">
+          Dalam memulai suatu penelitian, peneliti tentu perlu menentukan sampel
+          penelitiannya. Salah satu hal yang paling penting untuk ditentukan
+          adalah besar sampel minimal dalam penelitian agar suatu penelitian
+          dikatakan memiliki hasil yang valid.
+        </p>
+        <p className="subsection__text">
+          Besar sampel minimal ini dapat ditentukan dengan berbagai rumus.
+          Masing-masing penelitian memiliki rumus besar sampel. Peneliti perlu
+          mengetahui rumus besar sampel yang sesuai untuk mencapai penelitian
+          yang optimal.
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default BesarSampel;
