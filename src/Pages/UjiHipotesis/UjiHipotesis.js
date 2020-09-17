8a03d1141f8002f6d7bb0d31afa4af2d9410e19e
febriliankr <febrilian.kr@gmail.com>
2020-09-17 13:09:56 +0700

    fixed state management for questions

--- src/Pages/UjiHipotesis/UjiHipotesis.js
+++ src/Pages/UjiHipotesis/UjiHipotesis.js
@@ -4,41 +4,46 @@ import WelcomeText from "./components/WelcomeText";
 import Questions from "./components/Questions";
 
 function UjiHipotesis() {
-
   const [mulai, setMulai] = useState(false);
-
+  const [answers, setAnswers] = useState({
+    1: '',
+    2: '',
+    3: '',
+  });
   const [questions, setQuestions] = useState({
-    '1': {
+    1: {
       number: 1,
       question: "Berapa jumlah variabel bebas Anda?",
       options: ["Satu", "Dua atau lebih"],
-      answer: [],
+      answer: "",
     },
-    '2': {
+    2: {
       number: 2,
-      question: "Apakah variabel bebasmu terdiri dari 2 kelompok atau lebih dari 2 kelompok?",
+      question:
+        "Apakah variabel bebasmu terdiri dari 2 kelompok atau lebih dari 2 kelompok?",
       options: ["Dua kelompok (Dikotom)", "Dua atau lebih (Polikotom)"],
-      answer: [],
+      answer: "",
     },
-    '3': {
+    3: {
       number: 3,
       question: "Apakah variabel bebasmu berpasangan atau tidak berpasangan?",
       options: ["Satu", "Dua atau lebih"],
-      answer: [],
+      answer: "",
     },
   });
-
 
   return (
     <div className="uji-hipotesis__container">
       {mulai ? (
         Object.keys(questions).map((q) => {
-
           return (
             <>
               <Questions
                 number={questions[q].number}
-                setAnswers={setQuestions}
+                setQuestions={setQuestions}
+                questions={questions}
+                answers={answers}
+                setAnswers={setAnswers}
                 question={questions[q].question}
                 options={questions[q].options}
               />
@@ -48,7 +53,7 @@ function UjiHipotesis() {
       ) : (
         <WelcomeText setMulai={setMulai} />
       )}
-{/* 
+      {/* 
       {Object.keys(questions).map((q) => {
         return (
           <>
--- src/Pages/UjiHipotesis/components/Questions.js
+++ src/Pages/UjiHipotesis/components/Questions.js
@@ -1,24 +1,39 @@
 import React from "react";
 import "./Questions.css";
-import { Button } from "../../../components/Button/Button";
 import ReactTooltip from "react-tooltip";
 import { Link } from "react-router-dom";
 
-function Questions({ number, question, options }) {
 
-  const handleClick = (e) => {
-    console.log(e.target.innerHTML);
-  }
+
+function Questions({ number, question, options, setQuestions, questions, answers, setAnswers }) {
+  
+
+  const handleClick = (option, number) => {
+    console.log("option: ", option);
+    console.log("number: ", number);
+    // answers[number] = option;
+    // console.log("answers", answers[1], answers[2], answers[3]);
+    setAnswers({
+      ...answers,
+      [number]: option,
+    })
+
+    console.log('As: ', answers);
+
+  };
 
   return (
     <div className="questions">
       <ReactTooltip />
-      <p style={{
-        color: 'grey',
-        marginBottom: 0,
-      }}>Pertanyaan ke-{number}</p>
+      <p
+        style={{
+          color: "grey",
+          marginBottom: 0,
+        }}
+      >
+        Pertanyaan ke-{number}
+      </p>
       <div className="questions__head">
-        
         <h1 className="questions__title">{question}</h1>
         <div className="questions__ask">
           <Link
@@ -33,7 +48,16 @@ function Questions({ number, question, options }) {
         {options.map((option) => {
           return (
             <div className="questions__options">
-              <Button buttonStyle="btn--evoblue" onClick={handleClick}>{option}</Button>
+              <div
+                className={
+                  (answers[number] === option)
+                    ? `active questions__button`
+                    : `questions__button`
+                }
+                onClick={() => handleClick(option, number)}
+              >
+                {option}
+              </div>
             </div>
           );
         })}
@@ -43,3 +67,6 @@ function Questions({ number, question, options }) {
 }
 
 export default Questions;
+
+// {/* <div className={selected ? (`questions__options`): (`questions__options active`)}> */}
+// render 'active' class kalo answers[number] == option
