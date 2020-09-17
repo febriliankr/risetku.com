196a36bfeec233b03edffbf8323e99901c6e0cd9
febriliankr <febrilian.kr@gmail.com>
2020-09-17 16:14:30 +0700

    fixed uji hipotesis questions

--- src/Pages/UjiHipotesis/dataQuestions.js
+++ src/Pages/UjiHipotesis/dataQuestions.js
@@ -7,26 +7,32 @@ const dataQuestions = {
   2: {
     number: 2,
     question:
-      "Apakah variabel bebas Anda terdiri dari 2 kelompok atau lebih dari 2 kelompok?",
-    options: ["Dua kelompok (Dikotom)", "Dua atau lebih (Polikotom)"],
+      "Apakah jenis variabel bebasmu?",
+      options: ["Kategorik", "Numerik"],
   },
   3: {
     number: 3,
-    question: "Apakah variabel bebas Anda berpasangan atau tidak berpasangan?",
-    options: ["Satu", "Dua atau lebih"],
+    question:
+      "Apakah variabel bebas Anda terdiri dari 2 kelompok atau lebih dari 2 kelompok?",
+    options: ["Dua kelompok (Dikotom)", "Dua atau lebih (Polikotom)"],
   },
   4: {
     number: 4,
-    question: "Berapakah jumlah variabel terikat Anda?",
+    question: "Apakah variabel bebas Anda berpasangan atau tidak berpasangan?",
     options: ["Satu", "Dua atau lebih"],
   },
   5: {
     number: 5,
-    question: "Apakah jenis variabel terikat Anda?",
-    options: ["Kategorik", "Numerik"],
+    question: "Berapakah jumlah variabel terikat Anda?",
+    options: ["Satu", "Dua atau lebih"],
   },
   6: {
     number: 6,
+    question: "Apakah jenis variabel terikat Anda?",
+    options: ["Kategorik", "Numerik"],
+  },
+  7: {
+    number: 7,
     question: "Apakah data numerik Anda berdistribusi normal?",
     options: ["Ya", "Tidak"],
   },
